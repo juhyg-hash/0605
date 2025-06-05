a9537649fc3ec3f52b953ec2f57eb3d43ff71fc9
juhyg-hash <gusrb1474@gmail.com>
2025-06-05 11:05:18 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        //break 문
-//        for(int i = 1; i < 11; i++){
-//            if(i ==7){
-//                break;
-//            }
-//
-//            System.out.println(i);
-//        }
+        int a = 10;
+        int b = 13;
 
-//        for(int i= 1; i < 11; i++){
-//            //3의배수가아닐때
-//            if(i % 3 != 0){
-//                continue;
-//
-//            }
-//
-//            System.out.println(i);
-
-            for(int i =1; i <= 20 ; i++){
-                if( i >= 15){
-                break;
-            }
-                //5의 배수
-                if(i % 5 == 0){
-                    continue;
-                }
-                System.out.println(i);
-
+//        int max = (조건) ? 참 : 거짓
+        int max = (a > b) ? a : b;
+        System.out.println("max = " + max);
+        int max3; //여기서 max의 값을 출력을 하려 하면 에러가 난다 자바에서는 1.에러 2. 빈값 3.0
+        if (a > b) {
+            max3 = a;
+        } else {
+            max3 = b;
         }
+        System.out.println(max3);
+        }
+
     }
-}
