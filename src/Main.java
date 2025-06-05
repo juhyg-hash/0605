7fe3c95ce4dbbb23fb65e37b6c586265e7ba2f66
juhyg-hash <gusrb1474@gmail.com>
2025-06-05 10:14:08 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,25 @@ public class Main {
 //            System.out.println(i);
 //        }
 
-        for(int i= 1; i < 11; i++){
-            //3의배수가아닐때
-            if(i % 3 != 0){
-                continue;
+//        for(int i= 1; i < 11; i++){
+//            //3의배수가아닐때
+//            if(i % 3 != 0){
+//                continue;
+//
+//            }
+//
+//            System.out.println(i);
 
+            for(int i =1; i <= 20 ; i++){
+                if( i >= 15){
+                break;
             }
+                //5의 배수
+                if(i % 5 == 0){
+                    continue;
+                }
+                System.out.println(i);
 
-            System.out.println(i);
         }
     }
 }
