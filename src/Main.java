2a71a3bc7ae06a369a1361b611b0ff61a6b6c7f9
juhyg-hash <gusrb1474@gmail.com>
2025-06-05 11:44:44 +0900

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-//        int a = 10;
-//        int b = 13;
+        String color = "파랑";
 //
-//////        int max = (조건) ? 참 : 거짓
-//        int max = (a > b) ? a : b;
-//        System.out.println("max = " + max);
-//        int max3; //여기서 max의 값을 출력을 하려 하면 에러가 난다 자바에서는 1.에러 2. 빈값 3.0
-//        if (a > b) {
-//            max3 = a;
-//        } else {
-//            max3 = b;
-//        }
-//        System.out.println(max3);
-        int score = 61;
-        String resulet = (score > 60) ? "통과" : "실패";
-        System.out.println("resulet = " + resulet);
-        //-------------------------------------->if로 바꾸기
-        int score_1 = 63;
-        String result;
-        
-        if(score_1 > 60){
-            result = "통과";
-        }   else {
-            result = "실패";
+//        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : "보라";
+//        //가독성이 떨어짐
+//        System.out.println(choiceColor);
+//
+        if(color == "빨강") {
+            System.out.println("빨강");
+        }else if(color == "파랑") {
+            System.out.println("파랑");
         }
-        System.out.println("result = " + result);
-    }
+            else {
+                System.out.println("보라");
+        }
+    } //삼항 연산자는 중첩해서 사용 x
 }
-     
-
