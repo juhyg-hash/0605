bdc271fa6c030d817c9c1716c0da7e1dd699dfde
juhyg-hash <gusrb1474@gmail.com>
2025-06-05 14:37:38 +0900

    sixth commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 public class Main {
-    public static void printStudent(String name, int age) {
-        System.out.println("이름 = " + name);
-        System.out.println("나이 = " + age);
-        System.out.println("------------------");
+    public static int addNum(int a, int b){
+        int sum = a + b;
+        return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println("=== 학생 정보 출력 ===");
-        //Main.printStudent--->생략하는 이유는 같은 클래스 안에 있기 때문에
-        printStudent("Alice",20);
-        Main.printStudent("Bob",30);
-        printStudent("Charlie", 32);
+        int number = addNum(10,7);
 
+        System.out.println("number = " + number);
     }
 }
